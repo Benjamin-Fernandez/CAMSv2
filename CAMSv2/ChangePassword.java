949e2b05268c0fa83d15888bfffaf1a243928d28
JoshuaYup <joshuayap2000@gmail.com>
2023-11-18 09:53:34 +0800

    0953

--- CAMSv2/ChangePassword.java
+++
@@ -1,9 +0,0 @@
-package CAMSv2;
-
-public class ChangePassword {
-   static public void updatePassword(String NewPassword,String role){
-       //same logic to check role
-       // if true, then can update
-
-   }
-}
