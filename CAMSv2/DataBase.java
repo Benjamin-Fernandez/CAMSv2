e08ffa33e6a6bb0f07d9135cbf83a53b9414118e
hokkienmeeeee <122872717+hokkienmeeeee@users.noreply.github.com>
2023-11-20 15:40:03 +0800

    Update DataBase.java

--- CAMSv2/DataBase.java
+++ CAMSv2/DataBase.java
@@ -2,6 +2,16 @@ package CAMSv2;
 
 
 // change to interface
+/**
+ * 
+ * The DataBase abstract class provides a blueprint for database classes in the CAMSv2 system.
+ * It defines two key operations that all database classes must implement: loadToCSV and writeToCSV.
+ * The loadToCSV method is responsible for loading data from a CSV file into the database.
+ * The writeToCSV method is responsible for writing the current state of the database to a CSV file.
+ * This class follows the Singleton design pattern to ensure that only one instance of each database class exists in the system.
+ * @author Benjamin Fernandez
+ * @since 13-11-2023
+ */
 public abstract class DataBase{
     // Singleton
 
